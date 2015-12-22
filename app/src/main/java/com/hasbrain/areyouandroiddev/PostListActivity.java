decd55e37b26c6bf102da0818870cdcebe107ddb
Khanh ULtron <congkhanh197@gmail.com>
2015-12-22 21:43:22 +0700

    Exercise 2.2

--- app/src/main/java/com/hasbrain/areyouandroiddev/PostListActivity.java
+++ app/src/main/java/com/hasbrain/areyouandroiddev/PostListActivity.java
@@ -9,17 +9,22 @@ import com.hasbrain.areyouandroiddev.datastore.FileBasedFeedDataStore;
 import com.hasbrain.areyouandroiddev.model.RedditPost;
 import com.hasbrain.areyouandroiddev.model.RedditPostConverter;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +33,8 @@ public class PostListActivity extends AppCompatActivity {
     ArrayList<RedditPost> redditPosts;
     ListViewAdapter lvAdapter;
     ListView listView;
+    GridView gridView;
+    private boolean isLandscape;
 
     public static final String DATA_JSON_FILE_NAME = "data.json";
     private FeedDataStore feedDataStore;
@@ -37,6 +44,7 @@ public class PostListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResource());
         listView = (ListView) findViewById(R.id.list);
+        gridView = (GridView) findViewById(R.id.grid);
         GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.registerTypeAdapter(RedditPost.class, new RedditPostConverter());
         Gson gson = gsonBuilder.create();
@@ -66,9 +74,24 @@ public class PostListActivity extends AppCompatActivity {
     }
 
     protected void displayPostList(final List<RedditPost> postList) {
-        lvAdapter = new ListViewAdapter(this, postList);
-        listView.setAdapter(lvAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        isLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        lvAdapter = new ListViewAdapter(this, postList, isLandscape);
+
+        if (!isLandscape) {
+            listView.setAdapter(lvAdapter);
+//            setClick(listView, postList);
+            listView.setOnItemClickListener(new OnPostClickListener(postList, this));
+        }else {
+
+            gridView.setAdapter(lvAdapter);
+            setClick(gridView,postList);
+
+
+        }
+
+    }
+    protected void setClick (AbsListView view,final List<RedditPost> postList){
+        view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent myIntent = new Intent(view.getContext(), PostViewActivity.class);
                 myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -81,6 +104,28 @@ public class PostListActivity extends AppCompatActivity {
             }
         });
 
+    }
+    //TODO: static class vs non-static class??? final???
+    private static class OnPostClickListener implements AdapterView.OnItemClickListener {
+        private List<RedditPost> postList;
+        private Context context;
+
+        public OnPostClickListener(List<RedditPost> postList, Context context) {
+            this.postList = postList;
+            this.context = context;
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Intent myIntent = new Intent(view.getContext(), PostViewActivity.class);
+            myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            if (position != postList.size()) {
+                myIntent.putExtra("url", postList.get(position).getUrl());
+            } else {
+                myIntent.putExtra("url", context.getString(R.string.url));
+            }
+            context.startActivity(myIntent);
+        }
     }
 
     protected int getLayoutResource() {
--- app/src/main/java/com/hasbrain/areyouandroiddev/adapter/ListViewAdapter.java
+++ app/src/main/java/com/hasbrain/areyouandroiddev/adapter/ListViewAdapter.java
@@ -5,28 +5,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.hasbrain.areyouandroiddev.R;
 import com.hasbrain.areyouandroiddev.model.RedditPost;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Khanh Ultron on 12/17/2015.
  */
 public class ListViewAdapter extends ArrayAdapter<RedditPost> {
-    public static final int TYPE_FOOTER_ITEM = 0;
-    public static final int TYPE_NORMAL_ITEM = 1;
+    public static final int TYPE_LAST_ITEM = 0;
+    public static final int TYPE_TEXT_ITEM = 1;
     private Context context;
     private List<RedditPost> redditPosts;
+    private Boolean isLandscape;
 
-    public ListViewAdapter(Context context, List<RedditPost> redditPosts) {
+    public ListViewAdapter(Context context, List<RedditPost> redditPosts, Boolean isLandscape) {
         super(context, 0, redditPosts);
         this.context = context;
         this.redditPosts = redditPosts;
+        this.isLandscape = isLandscape;
     }
 
     @Override
@@ -42,8 +42,8 @@ public class ListViewAdapter extends ArrayAdapter<RedditPost> {
     @Override
     public int getItemViewType(int position) {
         if (position == getCount() - 1)
-            return TYPE_NORMAL_ITEM;
-        else return TYPE_FOOTER_ITEM;
+            return TYPE_TEXT_ITEM;
+        else return TYPE_LAST_ITEM;
 
         // return position % 2 == 0 ? 0 : 1;
     }
@@ -55,10 +55,10 @@ public class ListViewAdapter extends ArrayAdapter<RedditPost> {
         if (v == null) {
             LayoutInflater inflater = (LayoutInflater)
                     context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            if (type == TYPE_NORMAL_ITEM) {
-                v = inflater.inflate(R.layout.text_view, null);
+            if (type == TYPE_TEXT_ITEM) {
+                v = inflater.inflate(R.layout.view_end_layout, null);
             } else {
-                v = inflater.inflate(R.layout.type_view, null);
+                v = inflater.inflate(R.layout.type_item_view, parent, false);
             }
 
 //            v.setOnClickListener(new View.OnClickListener() {
@@ -68,32 +68,49 @@ public class ListViewAdapter extends ArrayAdapter<RedditPost> {
 //                }
 //            });
         }
-        if (type != TYPE_NORMAL_ITEM) {
+        if (type != TYPE_TEXT_ITEM) {
             RedditPost redditPost = redditPosts.get(position);
 
-            if (redditPost != null) {
-                TextView tv_score = (TextView) v.findViewById(R.id.tv_score);
-                tv_score.setText(redditPost.getScore() + "");
-                TextView tv_au = (TextView) v.findViewById(R.id.tv_author);
-                tv_au.setText(redditPost.getAuthor());
-                tv_au.setTextColor(0xff0a295a);
-                TextView tv_sub = (TextView) v.findViewById(R.id.tv_subreddit);
-                tv_sub.setText(redditPost.getSubreddit());
-                tv_sub.setTextColor(0xff0a295a);
+            if (!isLandscape) {
+                if (redditPost != null) {
+                    TextView tv_score = (TextView) v.findViewById(R.id.tv_score);
+                    tv_score.setText(redditPost.getScore() + "");
+                    TextView tv_au = (TextView) v.findViewById(R.id.tv_author);
+                    tv_au.setText(redditPost.getAuthor());
+                    tv_au.setTextColor(0xff0a295a);
+                    TextView tv_sub = (TextView) v.findViewById(R.id.tv_subreddit);
+                    tv_sub.setText(redditPost.getSubreddit());
+                    tv_sub.setTextColor(0xff0a295a);
 
-                TextView tv_tittle = (TextView) v.findViewById(R.id.tv_title);
-                tv_tittle.setText(redditPost.getTitle());
-                if (redditPost.isStickyPost())
-                    tv_tittle.setTextColor(0xff387801);
-                else tv_tittle.setTextColor(0xff000000);
-                TextView tv_cmt_dm_cd = (TextView) v.findViewById(R.id.tv_comment_domain_createdutc);
-                tv_cmt_dm_cd.setText(redditPost.getCommentCount() + " • " + redditPost.getDomain() + " • " + redditPost.getCreatedUTC());
+                    TextView tv_tittle = (TextView) v.findViewById(R.id.tv_title);
+                    tv_tittle.setText(redditPost.getTitle());
+                    if (redditPost.isStickyPost())
+                        tv_tittle.setTextColor(0xff387801);
+                    else tv_tittle.setTextColor(0xff000000);
+                    TextView tv_cmt_dm_cd = (TextView) v.findViewById(R.id.tv_comment_domain_createdutc);
+                    tv_cmt_dm_cd.setText(redditPost.getCommentCount() + " • " + redditPost.getDomain() + " • " + redditPost.getCreatedUTC());
+                }
+            } else {
+                if (redditPost != null) {
+                    TextView tv_score = (TextView) v.findViewById(R.id.tv_score);
+                    tv_score.setText(redditPost.getScore() + "");
+                    TextView tv_au = (TextView) v.findViewById(R.id.tv_author);
+                    tv_au.setText(redditPost.getAuthor());
+                    TextView tv_tittle = (TextView) v.findViewById(R.id.tv_title);
+                    tv_tittle.setText(redditPost.getTitle());
+                    if (redditPost.isStickyPost())
+                        tv_tittle.setTextColor(0xff387801);
+                    TextView tv_cmt_dm_cd = (TextView) v.findViewById(R.id.tv_comment_domain_createdutc);
+                    tv_cmt_dm_cd.setText(redditPost.getCommentCount() + " • " + redditPost.getDomain() + " • " + redditPost.getCreatedUTC());
+                }
             }
-        }
 
+
+        }
+        //TODO
         //ViewHolder pattern? why do we need to use viewholder?
         //LayoutInflater: meaning of  `parent` argument
-        //
+        //Inner class, anonymous class
 
         return v;
     }
